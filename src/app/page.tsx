af8dc1942183f9c463456906359e136d7b6dd288
remmi755 <90457721+remmi755@users.noreply.github.com>
2024-01-31 14:55:04 +0200

    add dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import Slider from "@/components/Slider";
 import Featured from "@/components/Featured";
 import Offer from "@/components/Offer";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 export default function Home() {
   return (
     <main>
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
 const ProductPage = () => {
   return <div>ProductPage</div>;
 };
